11f6035dfbc8115528be1aa683db5b7e54ecb1a6
fajrul aulia <fajrul.aulia@citra.digital>
2021-04-04 04:42:15 +0700

    First Commit - Create for Exporter PDf to SVG

---
+++ gofrompdf.go
@@ -0,0 +1,39 @@
+package gofrompdf
+
+import (
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+)
+
+type App struct{}
+
+func ExportToSVG(source string, newFile string) error {
+	var c *App
+	if !c.isValidFile(source) {
+		return errors.New("Invalid Source File")
+	}
+	if len(newFile) < 1 {
+		return errors.New("Invalid New File")
+	}
+	c.command(`inkscape --file="` + source + `" --export-plain-svg="` + newFile + `.svg" --export-margin=100 --export-margin=200 --export-margin=300`)
+	return nil
+}
+
+func (c *App) command(command string) (string, error) {
+	cmd := exec.Command("bash", "-c", command)
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	return string(stdout), nil
+}
+
+func (c *App) isValidFile(path string) bool {
+	if _, err := os.Stat(path); err != nil {
+		return false
+	}
+	return true
+}
---
+++ gofrompdf_test.go
@@ -0,0 +1,37 @@
+package gofrompdf
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommand(t *testing.T) {
+	var app App
+	str, err := app.command("apt | grep apt  | head -n 1 -c 3")
+	assert.Equal(t, err, nil, "Expected not error")
+	assert.Equal(t, str, "apt")
+}
+func TestIsValidFile(t *testing.T) {
+	var app App
+	err := ioutil.WriteFile("kucing.pdf", []byte("Hello"), 0755)
+	assert.Equal(t, err, nil, "Expected not error")
+	result := app.isValidFile("kucing.pdf")
+	assert.Equal(t, result, true, "expect true")
+	err = os.Remove("kucing.pdf")
+	assert.Equal(t, err, nil, "expect true")
+	result = app.isValidFile("kucing.pdf")
+	assert.Equal(t, result, false, "expect false")
+}
+
+func TestCreateFile(t *testing.T) {
+	err := ExportToSVG("contoh.pdf", "newSvgLetter")
+	assert.Equal(t, err, nil, "Expected not error")
+	var app App
+	result := app.isValidFile("newSvgLetter.svg")
+	assert.Equal(t, result, true, "expect false")
+	err = os.Remove("newSvgLetter.svg")
+	assert.Equal(t, err, nil, "expect true")
+}
